4bbf626ec79d1eb984891f20afbbff1cfe5308ef
nate eizenga <eizengan@gmail.com>
2016-10-20 12:38:38 -0400

    Enhancing configurability of inject-options, refactoring

--- inject-options.js
+++ inject-options.js
@@ -1,29 +1,62 @@
+/*
+
+  Adds injectOptions function to jQuery objects
+  Injects configurable <option> tags into specified dom objects
+
+*/
+
 (function($) {
-	$.fn.injectOptions = function(opts) {
-      opts=opts.map(function(opt) {
-      	if (typeof opt === 'string') return '<option>'+opt+'</option>';
-        else return '<option value="'+opt.key+'">'+opt.val+'</option>';
+  $.fn.injectOptions = function(items, settings) {
+    settings= $.extend({}, $.fn.injectOptions.defaults, settings);
+    return this.append(items.map(item => {
+      return $("<option>", {
+        value: item[settings.value],
+        disabled: item[settings.disabled] ? "disabled": undefined,
+        text: (typeof item === 'string') ? item : item[settings.text]
       });
-    	var optHTML = opts.reduce(function(html, opt) { return html+opt; });
-  	this.each(function() {
-    	$(this).html(optHTML);
-    });
-    return this;
+    }));
+  };
+  $.fn.injectOptions.defaults = {
+    value: "value",
+    disabled: "disabled",
+    text: "text"
   };
 }(jQuery));
 
-/*
+/******************
+*  USAGE EXAMPLES *
+*******************
 
-EXAMPLE
 
-var arr = ["foo", "bar", "baz", "faz"];
-var arr2 = [
-  { key: 1, val: "qux"},
-  { key: 2, val: "quux"},
-  { key: 3, val: "quuux"},
+//using an array of strings
+var strArray = ["foo", "bar", "baz"];
+$(".str-options").injectOptions(strArray);
+
+//using an array of objects (properties match defaults)
+var defArray = [
+  { value: 1, text: "foo"},
+  { value: 2, text: "bar", disabled: true},
+  { value: 3, text: "baz"}
 ];
+$(".def-options").injectOptions(defArray);
 
-$(".foo").injectOptions(arr);
-$(".qux").injectOptions(arr2);
+//using an array of objects (properties match user-defined defaults)
+var userDefArray = [
+  { key: 1, val: "foo"},
+  { key: 2, val: "bar", off: true},
+  { key: 3, val: "baz"}
+];
+$.fn.injectOptions.defaults.value = "key";
+$.fn.injectOptions.defaults.text = "val";
+$.fn.injectOptions.defaults.disabled = "off";
+$(".udef-options").injectOptions(userDefArray);
+
+//manually specifying the object properties in the settings argument
+var userArray = [
+  { num: 1, data: "foo"},
+  { num: 2, data: "bar", inactive: true},
+  { num: 3, data: "baz"}
+];
+$(".user-options").injectOptions(userArray, { value: "num", text: "data", disabled: "inactive"});
 
 */
